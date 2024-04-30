aa441f1afbfe638cd41f8a4021f878cc99b12b9d
Taylor Tam <taylor@taylortam.com>
2024-04-29 17:28:43 -0700

    works using open ai!

--- analyzer.py
+++ analyzer.py
@@ -3,11 +3,18 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OllamaEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-
+from langchain_community.llms import OpenAI
+import configparser
 
 class Analyzer():
     def __init__(self):
         self.hasty_generalizations = ["In society", "In the modern day", "Since the beginning of time", "Some may argue"]
+        self.api_key = self.load_api_key()
+
+    def load_api_key(self):
+        config = configparser.ConfigParser()
+        config.read('config.ini') 
+        return config['API']['APIKey']
 
     def process_data(self, text):
         print("processing data...")
@@ -21,7 +28,7 @@ class Analyzer():
         all_splits = text_splitter.split_documents(data)
         oembed = OllamaEmbeddings(model="nomic-embed-text")
         vectorstore = Chroma.from_documents(documents=all_splits, embedding=oembed)
-        question="What is Porath's purpose?"
-        print("done")
-        # qachain=RetrievalQA.from_chain_type(ollama, retriever=vectorstore.as_retriever())
-        # qachain.invoke({"query": question})
+        question="Return the entire input text with double asterisks around the phrase [we begin to wonder]"
+        qachain=RetrievalQA.from_chain_type(llm=OpenAI(api_key=self.api_key), retriever=vectorstore.as_retriever())
+        ans = qachain.invoke({"query": question})
+        print(ans["result"])
