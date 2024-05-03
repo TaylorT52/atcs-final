35e1416af624d43cb74bc8a194c43410fa21f84c
Taylor Tam <taylor@taylortam.com>
2024-05-03 14:14:47 -0700

    highlighting displays error

--- analyzer.py
+++ analyzer.py
@@ -5,14 +5,14 @@ from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain_community.llms import OpenAI
 from langchain_community.llms import Ollama
-import configparser
 from crewai import Agent, Task, Crew, Process
 
 class Analyzer():
     def __init__(self):
         self.hasty_generalizations = ["In society", "In the modern day", "Since the beginning of time", "Some may argue"]
+        self.tvw = ["Necessity of", "important", "importance of", "value of", "useful", "necessary"]
         self.model = Ollama(model = "llama2")
-        self.goal = "Return the exact sentences with 'importance of, significance of, value of, valuable, useful, necessary, necessity of, important, it's important, crucial' or similar errors in this text that tell readers 'that' something matters and not 'what' matters. Exclude sentences that contain 'because' or are statements of fact."
+        self.goal = "Return the exact sentences with 'importance of, significance of, value of, valuable, useful, necessary, necessity of, important, it's important, crucial' or similar errors in this text that tell readers 'that' something matters and not 'what' matters."
 
     def process_data(self, text):
         responder = Agent(
--- integ.py
+++ integ.py
@@ -14,32 +14,41 @@ class UIClass:
         self.HEIGHT = 700
         self.PANELWIDTH = 350
         self.essay = ""
+        self.matched_errors = []
+        self.matched_sentences = []
         self.selected_sentence = "Click on any highlighted sentence to see your error"
         self.dropdown_options = ["Option 1", "Option 2", "Option 3"]
-        self.acceptable = [",", "-", ";", ":", "'", "’", " "]
         self.create_ui()
 
+    ### manage UI here ### 
     def create_ui(self):
         self.root.title("Essay Annihilator")
         self.root.geometry(f"{self.WIDTH}x{self.HEIGHT}")
+        self.root.resizable(False, False)
+
+        ### LEFT PANEL ###
         left_panel = tk.Frame(self.root, bg='white')
+        left_panel.grid_propagate(False)
         left_panel.pack(side='left', fill='both', expand=True)
         self.text_area = ScrolledText(left_panel, wrap=tk.WORD, bg="white", font=('Times New Roman', 18), foreground="black")
         self.text_area.pack(padx=10, pady=10, fill='both', expand=True)
+
+        ### RIGHT PANEL ###
         right_panel = tk.Frame(self.root, bg='lightgray', width=self.PANELWIDTH)
         right_panel.pack(side='right', fill='both', expand=True)
-        title0 = ttk.Label(right_panel, text="Analyze")
-        title0.pack(pady=10)
+        right_panel.grid_propagate(False)
+        title0 = ttk.Label(right_panel, text="Analyze", font=('Times New Roman', 18)).pack(pady=10)
         upload_button = tk.Button(right_panel, text='Upload Text File', bg="lightgray", fg="black", highlightbackground='black',
                                   font=('Times New Roman', 18), command=lambda: self.upload_file(self.text_area))
         upload_button.pack(pady=25, padx=20)
         analyze_button = tk.Button(right_panel, text='Analyze Text', bg="lightgray", fg="black", highlightbackground='black',
                                    font=('Times New Roman', 18), command=lambda: self.analyze_text(self.text_area))
         analyze_button.pack(pady=10, padx=20)
-        title1 = ttk.Label(right_panel, text=self.selected_sentence)
-        title1.pack(pady=10)
+        self.title1 = ttk.Label(right_panel, text=self.selected_sentence, font=('Times New Roman', 18)).pack(pady=20)
+        self.title2 = ttk.Label(right_panel, text="Add to the model", font=('Times New Roman', 18)).pack(pady=20)
         self.root.mainloop()
 
+    ### upload file as a .txt and read ### 
     def upload_file(self, text_widget):
         file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
         if file_path:
@@ -49,13 +58,20 @@ class UIClass:
                 text_widget.delete('1.0', tk.END)
                 text_widget.insert(tk.END, content)
 
+    ### highlight sentences here ### 
     def analyze_text(self, text_widget):
         results = self.analyzer.process_data(self.essay)
-        pattern = r'\d+\.\s+"(.*?)"'
+        pattern = r'\d+\.\s+"(.*?)"\s*(.*?)(?:\n|$)'
+        
+        ### find sentences in the text ###
+        matches = re.findall(pattern, results, re.DOTALL)
+        for match in matches:
+            sentence, after_sentence = match
+            self.matched_sentences.append(sentence)
+            self.matched_errors.append(after_sentence)
 
-        # Find all matches
-        result_sentences = re.findall(pattern, results, re.DOTALL)
-        for index, sentence in enumerate(result_sentences):
+        ### tag to highlight ###
+        for index, sentence in enumerate(self.matched_sentences):
             start_index = self.essay.find(sentence)
             if start_index != -1:
                 if not start_index == 0:
@@ -66,11 +82,11 @@ class UIClass:
                     text_widget.tag_add(tag_name, start_position, end_position)
                     text_widget.tag_config(tag_name, background='yellow', foreground='black')
 
-                    # Bind event to the tag
                     def on_click(event, s=sentence):
-                        print(f"Clicked on highlighted text: {s}")
-                        self.selected_sentence = s
-                        self.root.update()
+                        idx = self.matched_sentences.index(s)
+                        error = self.matched_errors[idx]
+                        self.title1.config(text=error)
+                        self.root.update_idletasks()
 
                     text_widget.tag_bind(tag_name, "<Button-1>", on_click)
             else:
